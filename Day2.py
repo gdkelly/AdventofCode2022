4c1d605f725ce0f4f9cb2b264b491d4084913839
gdkelly <80280327+gdkelly@users.noreply.github.com>
2022-12-02 16:57:34 +0000

    Day 2 part 2

---
+++ Day2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 16:37:44 2022
+
+@author: Greg
+"""
+
+import pandas as pd
+df=pd.read_csv('input.txt',header=None,sep=' ')
+score=0
+values={'X':{'init':0,'A':3,'B':1,'C':2},
+        'Y':{'init':3,'A':1,'B':2,'C':3},
+        'Z':{'init':6,'A':2,'B':3,'C':1}}
+for i in range(0,len(df)):
+    wld=df[1][i]
+    opponent_pick=df[0][i]
+    score+=values[wld]['init']+values[wld][opponent_pick]
+
+print(score)
